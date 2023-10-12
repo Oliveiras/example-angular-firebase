95cfd07506cea731d0eb8ec647174264087cf809
Alex Oliveira <ralex@portalmedico.org.br>
2023-10-12 19:48:59 -0300

    Retirandos problemas de compatibilidade de versões entre Angular, Firebase, AngularFire e RxFire

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-10923',
+    appId: '1:702911231720:web:42f16cb9f9d981f03c7b5d',
+    storageBucket: 'kanbanfire-10923.appspot.com',
+    apiKey: 'AIzaSyDKD72ooY3u2msDiQnHwlm5y5ywDpMbwdw',
+    authDomain: 'kanbanfire-10923.firebaseapp.com',
+    messagingSenderId: '702911231720',
+    measurementId: 'G-1QKVMSERWJ',
+  },
+};
