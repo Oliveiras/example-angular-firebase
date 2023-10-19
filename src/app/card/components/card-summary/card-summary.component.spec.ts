4d9587efeeb41c627e49e2bf9d73efd7f13f222e
Alex Oliveira <ralex@portalmedico.org.br>
2023-10-18 23:19:14 -0300

    Adjusting test of card-summary

--- src/app/card/components/card-summary/card-summary.component.spec.ts
+++ src/app/card/components/card-summary/card-summary.component.spec.ts
@@ -1,21 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CardSummaryComponent } from './card-summary.component';
+import {CardSummaryComponent} from './card-summary.component';
+import {Component, ViewChild} from "@angular/core";
+import {MatCardModule} from "@angular/material/card";
+
+@Component({
+  template: `<app-card-summary [title]="titulo" [description]="descricao"></app-card-summary>`
+})
+class TestCardSummaryComponent {
+  @ViewChild(CardSummaryComponent) public component!: CardSummaryComponent;
+  titulo = "Título";
+  descricao = "Descrição";
+}
 
 describe('CardSummaryComponent', () => {
+  let hostComponent: TestCardSummaryComponent;
   let component: CardSummaryComponent;
-  let fixture: ComponentFixture<CardSummaryComponent>;
+  let fixture: ComponentFixture<TestCardSummaryComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CardSummaryComponent]
-    });
-    fixture = TestBed.createComponent(CardSummaryComponent);
-    component = fixture.componentInstance;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        CardSummaryComponent,
+        TestCardSummaryComponent
+      ],
+      imports: [
+        MatCardModule
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestCardSummaryComponent);
     fixture.detectChanges();
+
+    hostComponent = fixture.componentInstance;
+    component = hostComponent.component;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('shoud receive title and desc', () => {
+    expect(component.title).toBe("Título");
+    expect(component.description).toBe("Descrição");
+  });
+
 });
